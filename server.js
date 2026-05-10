3fb266c4d41a6f35db59c686f7d97364b8e6ae2c
hoainam02720-ui <hoainam02720@gmail.com>
2026-05-10 14:16:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,14 @@
+
 const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-let donhang = [];
-
 app.get("/", (req, res) => {
-  res.send(`
-    <h1>Máy Bán Gạo 🌾</h1>
-
-    <form action="/dat" method="POST">
-
-      <p>Tên:</p>
-      <input name="ten" />
-
-      <p>Số điện thoại:</p>
-      <input name="sdt" />
-
-      <p>Địa chỉ giao:</p>
-      <input name="diachi" />
-
-      <p>Số kg gạo:</p>
-      <input name="soluong" />
-
-      <br><br>
-
-      <button type="submit">
-        ĐẶT GẠO
-      </button>
-
-    </form>
-  `);
-});
-
-app.post("/dat", (req, res) => {
-
-  donhang.push(req.body);
-
-  console.log("Có đơn mới:");
-  console.log(req.body);
-
-  res.send("Đặt gạo thành công 😄");
-
-});
-
-app.get("/donhang", (req, res) => {
-
-  res.json(donhang);
-
+  res.send("Máy bán gạo chạy rồi 😄");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-
-  console.log("Máy chủ đang chạy");
-
+  console.log("Server chạy");
 });
