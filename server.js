f4ff008ed619c08ab36f5496c3f44a5f0e2b1d8a
hoainam02720-ui <hoainam02720@gmail.com>
2026-05-10 13:32:43 +0700

    Initialize Express server with webhook endpoint

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let may1 = "WAIT";
+
+app.get("/", (req, res) => {
+    res.send("Máy bán gạo online 😄");
+});
+
+app.get("/may1", (req, res) => {
+    res.send(may1);
+});
+
+app.post("/webhook", (req, res) => {
+
+    console.log(req.body);
+
+    may1 = "OPEN";
+
+    setTimeout(() => {
+        may1 = "WAIT";
+    }, 5000);
+
+    res.send("OK");
+});
+
+app.listen(3000, () => {
+    console.log("Server running");
+});
